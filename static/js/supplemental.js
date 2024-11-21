8a7730e7bcd09b005310f4999137607444732117
Brian Chao <bchao1@meta.com>
2024-11-21 15:24:17 -0800

    fix video pause play logic

--- static/js/supplemental.js
+++ static/js/supplemental.js
@@ -1,42 +1,44 @@
 let currentScene = 'mipnerf360-bicycle';
 let currentModel = '3DGS';
 let currentTimeVaryGaussians = 0;
-
-let currentTextureAblationScene = 'mipnerf360-bicycle';
-let currentTexture = 'none';
-let currentTimeTextureAblation = 0;
+let videoPaused = false;
 
 let currentSceneDecomp = 'mipnerf360-bicycle';
 let currentTexDecomp = 'none';
 let currentColorDecomp = 'base';
 let currentTimeColorDecomp = 0;
+let videoPausedDecomp = false;
 
-let currentSceneVaryNumGSFixTexRes = 'mipnerf360-bicycle';
-let currentValueVaryNumGSFixTexRes = 1;
-let currentTimeVaryNumGSFixTexRes = 0;
 
-let currentSceneVaryTexResFixNumGS = 'mipnerf360-bicycle';
-let currentValueVaryTexResFixNumGS = 1;
-let currentTimeVaryTexResFixNumGS = 0;
-
-window.onload = function() {
-    currentTimeVaryGaussians = document.getElementById('video-vary-gaussians').currentTime;
-    currentTimeTextureAblation = document.getElementById('video-tex-ablation').currentTime;
-    currentTimeColorDecomp = document.getElementById('video-color-decomp').currentTime;
-};
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("video-vary-gaussians").addEventListener('pause', () => {
+    console.log("pause video");
+    videoPaused = true;
+  });
+  document.getElementById("video-vary-gaussians").addEventListener('play', () => {
+    videoPaused = false;
+  });
+  document.getElementById("video-color-decomp").addEventListener('pause', () => {
+    console.log("pause video");
+    videoPausedDecomp = true;
+  });
+  document.getElementById("video-color-decomp").addEventListener('play', () => {
+    videoPausedDecomp = false;
+  });
+  currentTimeVaryGaussians = document.getElementById('video-vary-gaussians').currentTime;
+  currentTimeColorDecomp = document.getElementById('video-color-decomp').currentTime;
+});
 
 // Varying number of Gaussians
 
 function selectScene(scene) {
   document.querySelectorAll('[id^="scene-button"]').forEach(button => button.classList.remove('is-active'));
   document.getElementById(`scene-button-${scene}`).classList.add('is-active');
-  document.getElementById('video-vary-gaussians').setAttribute("autoplay", "");
   currentScene = scene;
   updateColorVaryVideo();
 }
 
 function selectModel(model) {
-  document.getElementById('video-vary-gaussians').removeAttribute("autoplay");
   document.querySelectorAll('#model-button-3dgs, #model-button-ours').forEach(button => button.classList.remove('is-active'));
   if (model === '3DGS') {
     document.getElementById('model-button-3dgs').classList.add('is-active');
@@ -48,24 +50,28 @@ function selectModel(model) {
 }
 
 function updateColorVarySlider() {
-  document.getElementById('video-vary-gaussians').removeAttribute("autoplay");
-  let sliderValues = ['1', '2', '5', '10', '20', '50', '100'];
+  let sliderValues = ['1', '5', '20', '100'];
   currentValue = parseInt(document.getElementById('slider').value);
   document.getElementById('slider-value').innerHTML = "Percentage of Gaussians: " + sliderValues[currentValue - 1] + "%";
   updateColorVaryVideo()
 }
 
 function updateColorVaryVideo() {
-  let sliderValues = ['1', '2', '5', '10', '20', '50', '100'];
+  let sliderValues = ['1', '5', '20', '100'];
   currentValue = parseInt(document.getElementById('slider').value);
   let videoName = `${currentScene}_${currentModel}_${sliderValues[currentValue - 1]}.mp4`;
-  document.getElementById('video-source-vary-gaussians').src = `./static/compressed_videos_small/${videoName}`;
+  document.getElementById('video-source-vary-gaussians').src = `./static/videos/all/${videoName}`;
   currentTimeVaryGaussians = document.getElementById('video-vary-gaussians').currentTime;
   document.getElementById('video-vary-gaussians').load();
   document.getElementById('video-vary-gaussians').currentTime = currentTimeVaryGaussians;
+  if(videoPaused){
+    document.getElementById('video-vary-gaussians').pause();
+  } else {
+    document.getElementById('video-vary-gaussians').play();
+  }
 }
 
-
+/*
 // Vary Number of Gaussians with Fixed Texture Resolution
 function selectVaryNumGSFixTexResScene(scene) {
   document.querySelectorAll('#scene-vary-num-gs-fix-tex-res-button-mipnerf360-kitchen, #scene-vary-num-gs-fix-tex-res-button-mipnerf360-bicycle, #scene-vary-num-gs-fix-tex-res-button-mipnerf360-stump, #scene-vary-num-gs-fix-tex-res-button-custom-flower_gallery, #scene-vary-num-gs-fix-tex-res-button-custom-children_art').forEach(button => button.classList.remove('is-active'));
@@ -78,7 +84,7 @@ function updateVaryNumGSFixTexResVideo() {
   let sliderValues = ['1', '2', '5', '10', '20', '50', '100'];
   currentValueVaryNumGSFixTexRes = parseInt(document.getElementById('vary-num-gs-fix-tex-res-slider').value);
   let videoName = `${currentSceneVaryNumGSFixTexRes}_${sliderValues[currentValueVaryNumGSFixTexRes - 1]}.mp4`;
-  document.getElementById('video-source-vary-num-gs-fix-tex-res').src = `./static/compressed_videos_small/${videoName}`;
+  document.getElementById('video-source-vary-num-gs-fix-tex-res').src = `./static/videos/all/${videoName}`;
   currentTimeVaryNumGSFixTexRes = document.getElementById('video-vary-num-gs-fix-tex-res').currentTime;
   document.getElementById('video-vary-num-gs-fix-tex-res').load();
   document.getElementById('video-vary-num-gs-fix-tex-res').currentTime = currentTimeVaryNumGSFixTexRes;
@@ -98,7 +104,7 @@ function updateVaryTexResFixNumGSVideo() {
   let sliderValues = [2, 5, 10, 20, 30, 40];
   currentValueVaryTexResFixNumGS = parseInt(document.getElementById('vary-tex-res-fix-num-gs-slider').value);
   let videoName = `${currentSceneVaryTexResFixNumGS}_${sliderValues[currentValueVaryTexResFixNumGS - 1]}.mp4`;
-  document.getElementById('video-source-vary-tex-res-fix-num-gs').src = `./static/compressed_videos_small/${videoName}`;
+  document.getElementById('video-source-vary-tex-res-fix-num-gs').src = `./static/videos/all/${videoName}`;
   currentTimeVaryTexResFixNumGS = document.getElementById('video-vary-tex-res-fix-num-gs').currentTime;
   document.getElementById('video-vary-tex-res-fix-num-gs').load();
   document.getElementById('video-vary-tex-res-fix-num-gs').currentTime = currentTimeVaryTexResFixNumGS;
@@ -128,16 +134,16 @@ function selectTexture(texture) {
 function updateTexAblationVideo() {
   let videoName = `${currentTextureAblationScene}_${currentTexture}.mp4`;
   currentTimeTextureAblation = document.getElementById('video-tex-ablation').currentTime;
-  document.getElementById('video-source-tex-ablation').src = `./static/compressed_videos_small/${videoName}`;
+  document.getElementById('video-source-tex-ablation').src = `./static/videos/all/${videoName}`;
   document.getElementById('video-tex-ablation').load();
   document.getElementById('video-tex-ablation').currentTime = currentTimeTextureAblation;
   //document.getElementById('video-name-tex-ablation').innerHTML = videoName;
 }
+*/
 
 
 // Color component decomposition
 function selectSceneDecomp(scene) {
-  document.getElementById('video-color-decomp').setAttribute("autoplay", "");
   document.querySelectorAll('[id^="scene-decomp-button"]').forEach(button => button.classList.remove('is-active'));
   document.getElementById(`scene-decomp-button-${scene}`).classList.add('is-active');
   currentSceneDecomp = scene;
@@ -146,7 +152,6 @@ function selectSceneDecomp(scene) {
 
 
 function selectTexDecomp(texture) {
-  document.getElementById('video-color-decomp').removeAttribute("autoplay");
   document.querySelectorAll('#decomp-tex-button-none, #decomp-tex-button-alpha, #decomp-tex-button-rgb, #decomp-tex-button-rgba').forEach(button => button.classList.remove('is-active'));
   document.getElementById(`decomp-tex-button-${texture}`).classList.add('is-active');
   currentTexDecomp = texture;
@@ -154,8 +159,7 @@ function selectTexDecomp(texture) {
 }
 
 function selectColorDecomp(color) {
-  document.getElementById('video-color-decomp').removeAttribute("autoplay");
-  document.querySelectorAll('#decomp-color-button-base, #decomp-color-button-tex').forEach(button => button.classList.remove('is-active'));
+  document.querySelectorAll('#decomp-color-button-base, #decomp-color-button-tex, #decomp-color-button-final').forEach(button => button.classList.remove('is-active'));
   document.getElementById(`decomp-color-button-${color}`).classList.add('is-active');
   currentColorDecomp = color;
   updateColorDecompVideo();
@@ -163,11 +167,22 @@ function selectColorDecomp(color) {
 
 
 function updateColorDecompVideo() {
-  let videoName = `${currentSceneDecomp}_${currentTexDecomp}_${currentColorDecomp}.mp4`;
-  console.log(videoName);
+  let videoNameSuffix = currentColorDecomp;
+  if (currentColorDecomp == "final"){
+    if(currentTexDecomp == "none" || currentTexDecomp == "alpha"){
+      videoNameSuffix = "base";
+    }
+  }
+  let videoName = `${currentSceneDecomp}_${currentTexDecomp}_${videoNameSuffix}.mp4`;
+  //console.log(videoName);
   currentTimeColorDecomp = document.getElementById('video-color-decomp').currentTime;
-  document.getElementById('video-source-color-decomp').src = `./static/compressed_videos_small/${videoName}`;
+  document.getElementById('video-source-color-decomp').src = `./static/videos/all/${videoName}`;
   document.getElementById('video-color-decomp').load();
   document.getElementById('video-color-decomp').currentTime = currentTimeColorDecomp;
   //document.getElementById('video-name-color-decomp').innerHTML = videoName;
+  if(videoPausedDecomp){
+    document.getElementById('video-color-decomp').pause();
+  } else {
+    document.getElementById('video-color-decomp').play();
+  }
 }
